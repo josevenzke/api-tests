2991a98729dcc876ce02bb515f2d8835ff3632f3
josevenzke <josevenzkejr@gmail.com>
2021-06-05 18:34:52 -0300

    new add api endpoint, fix list endpoint

--- teste/serializers.py
+++ teste/serializers.py
@@ -1,7 +1,7 @@
 from .models import Person
 from rest_framework import serializers
 
-class PersonSerializer(serializers.HyperlinkedModelSerializer):
+class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = ['id','first_name','last_name']
+        fields = '__all__'
--- teste/views.py
+++ teste/views.py
@@ -1,10 +1,27 @@
 from .models import Person
-from rest_framework import viewsets
-from teste.serializers import PersonSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import PersonSerializer
+from teste import serializers
 
-class PersonViewSet(viewsets.ModelViewSet):
+@api_view(['GET'])
+def PersonList(request):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = Person.objects.values()
-    serializer_class = PersonSerializer
+    queryset = Person.objects.all()
+    serialize = PersonSerializer(queryset,many=True)
+
+    return Response(serialize.data)
+
+
+@api_view(['POST'])
+def PersonCreate(request):
+    first_name = request.POST.get('first_name')
+    last_name = request.POST.get('last_name')
+
+    person = Person.objects.create(first_name=first_name,last_name=last_name)
+    
+    serializer = PersonSerializer(person,many=False)
+
+    return Response(serializer.data)
