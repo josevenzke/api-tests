8f0000db67aeb9de8e9b9422b56f08e7e4250407
josevenzke <josevenzkejr@gmail.com>
2021-06-07 18:25:06 -0300

    added new space string endpoint,better variables names for all endpoints

--- modifystr/urls.py
+++ modifystr/urls.py
@@ -1,9 +1,12 @@
+from modifystr.serializers import ModifySerializer
 from django.urls import path
 from modifystr import views
 
 urlpatterns = [
+    path('', views.ModifyUrls, name='modify-urls'),
     path('string-list/', views.StringList, name='string-list'),
     path('string-reverse/', views.StringReverse, name='string-reverse'),
     path('string-randomize/', views.StringRandomize, name='string-randomize'),
+    path('string-space/', views.StringSpace, name='string-space'),
 
-]
+]   
--- modifystr/views.py
+++ modifystr/views.py
@@ -5,13 +5,34 @@ from rest_framework.response import Response
 from .serializers import ModifySerializer
 from modifystr import serializers
 
+
 # Create your views here.
 @api_view(['GET'])
+def ModifyUrls(request):
+    url_patterns = {
+        'string_list': 
+            {
+            'path': '/modify/string-list/',
+            'description': 'list all the strings ever made/modified',
+            'documentation': 'url_here'
+            }
+        }
+
+    return Response({'endpoints':url_patterns})
+
+
+@api_view(['GET'])
 def StringList(request):
-    queryset = ModifyStr.objects.all()
+    method = request.GET.get('method')
+    
+    kw = {}
+    if method:
+        kw['method'] = method
+
+    queryset = ModifyStr.objects.filter(**kw)
     serializer = ModifySerializer(queryset,many=True)
 
-    return Response(serializer.data)
+    return Response({'Success':True,'string_list':serializer.data,'method':method})
 
 @api_view(['POST'])
 def StringReverse(request):
@@ -21,13 +42,14 @@ def StringReverse(request):
     if case_sensitive:
         string = string.lower()
 
-    reversed_string = string[::-1]
-    modified = ModifyStr.objects.create(original_string=string,new_string=reversed_string,method='Reverse')
+    reversed = string[::-1]
+
+    modified = ModifyStr.objects.create(original_string=string,new_string=reversed,method='Reverse')
     modified.save()
 
     serializer = ModifySerializer(modified,many=False)
 
-    return Response({'Success':True,'reversed':reversed_string,'object':serializer.data})
+    return Response({'Success':True,'reversed':reversed,'object':serializer.data})
 
 @api_view(['POST'])
 def StringRandomize(request):
@@ -37,10 +59,32 @@ def StringRandomize(request):
     if case_sensitive:
         string = string.lower()
 
-    randomized_string = ''.join(random.sample(string,len(string)))
-    modified = ModifyStr.objects.create(original_string=string,new_string=randomized_string,method='Randomize')
+    randomized = ''.join(random.sample(string,len(string)))
+
+    modified = ModifyStr.objects.create(original_string=string,new_string=randomized,method='Randomize')
     modified.save()
 
     serializer = ModifySerializer(modified,many=False)
 
-    return Response({'Success':True,'randomized':randomized_string, 'object':serializer.data})
+    return Response({'Success':True,'randomized':randomized, 'object':serializer.data})
+
+@api_view(['POST'])
+def StringSpace(request):
+    case_sensitive = request.POST.get('case_sensitive')
+    string = request.POST.get('string')
+    char = request.POST.get('char')
+    if not all([char,string]):
+        return Response({'Success':False,'Error':'Missing params'})
+
+    if char not in string:
+        return Response({'Success':False,'Error':'Cant find any char in string'})
+
+    spaced_string = string.replace(char," ")
+
+    modified = ModifyStr.objects.create(original_string=string,new_string=spaced_string,method='Space')
+    modified.save()
+
+    serializer = ModifySerializer(modified,many=False)
+
+    return Response({'Success':True,'spaced':spaced_string,'object':serializer.data})
+
