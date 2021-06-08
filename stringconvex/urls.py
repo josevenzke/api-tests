84503de586ea0d3653fb68f60be5eb07baf06fa1
josevenzke <josevenzkejr@gmail.com>
2021-06-08 18:20:45 -0300

    fix modifystr path, and added new algo path

--- stringconvex/urls.py
+++ stringconvex/urls.py
@@ -16,5 +16,6 @@ Including another URLconf
 from django.urls import include, path
 
 urlpatterns = [
-    path('modify/', include('modifystr.urls')),
+    path('api/modify/', include('modifystr.urls')),
+    path('api/algo/',include('algo.urls')),
 ]
