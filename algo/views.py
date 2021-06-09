5db450691b7dac845e1d79d814badc197103c849
josevenzke <josevenzkejr@gmail.com>
2021-06-09 18:12:06 -0300

    using new binary_search helpers, and better sintaxe

--- algo/views.py
+++ algo/views.py
@@ -1,21 +1,23 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from lib import binarysearch_helper
+from lib import binarysearch_helper as binary
 # Create your views here.
 
 @api_view(['POST'])
 def binary_search(request):
-    array = request.POST.get('array').split(",")
-    item = request.POST.get('item')
-    array = [int(i) for i in array]
+    arrayString = request.POST.get('array')
+    itemString = request.POST.get('item')
 
+    array = binary.string_to_array(arrayString)
+    item = binary.string_to_int(itemString)
 
-    item = int(item)
-
-    index = binarysearch_helper.search(array,item)
-
-    return Response({'item_index':index})
+    if array and item:
+        index = binary.search(array,item)
+    else:
+        index = []
+        
+    return Response({'item_index':index,'array_lenght':len(array)})
 
 
 
