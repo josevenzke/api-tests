be0607d354cacb708a206a99351834c193901abc
josevenzke <josevenzkejr@gmail.com>
2021-06-09 18:59:53 -0300

    added binary-search and quicksort api endpoints

--- algo/urls.py
+++ algo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from algo import views
 
 urlpatterns = [
-    path('binary-search/', views.binary_search, name='binary-search')
+    path('binary-search/', views.binary_search, name='binary-search'),
+    path('quicksort-sort/',views.quicksort_sort, name='quicksort'),
+
 ]
