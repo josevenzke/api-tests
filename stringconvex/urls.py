a29f58bd1cdcc285ff7cba687fdd6b856acea58a
josevenzke <josevenzkejr@gmail.com>
2021-06-10 18:23:41 -0300

    added path for getting/refreshing auth tokens

--- stringconvex/urls.py
+++ stringconvex/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
+from django.contrib import admin
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
+    path('admin/',admin.site.urls),
     path('api/modify/', include('modifystr.urls')),
     path('api/algo/',include('algo.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
