f3cc91f746604caed875720ff5ade5cf9e61ee4f
josevenzke <josevenzkejr@gmail.com>
2021-06-14 18:55:39 -0300

    better sintaxe and error handling

--- lib/binarysearch_helper.py
+++ lib/binarysearch_helper.py
@@ -1,3 +1,4 @@
+
 def search(array, item):
     baixo = 0
     alto = len(array) - 1
@@ -14,21 +15,3 @@ def search(array, item):
             return meio
     
     return -1
-
-def string_to_array(string):
-    try:
-        arrayOfStrings = string.split(",") 
-        arrayOfIntegers = [int(i) for i in arrayOfStrings]
-    except Exception as e:
-        return e
-
-    return arrayOfIntegers
-
-def string_to_int(string):
-    try:
-        integer = int(string)
-    except Exception as e:
-        return e
-        
-    
-    return integer
