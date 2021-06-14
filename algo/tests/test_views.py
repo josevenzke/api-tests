c94b9ba7ce536e6e4ad274029fda87429ba25c22
josevenzke <josevenzkejr@gmail.com>
2021-06-14 18:10:01 -0300

    added tests for no params passed

--- algo/tests/test_views.py
+++ algo/tests/test_views.py
@@ -4,12 +4,23 @@ import pdb
 
 class TestViews(TestSetUp):
 
+    def test_algo_quicksort_no_data(self):
+        response = self.client.post(self.quicksort)
+        
+        self.assertEqual(response.data.get('Success'),False)
+
     def test_algo_quicksort(self):     
         response = self.client.post(self.quicksort,self.quicksort_array_data)
         quicksort_array = response.data.get('sorted_array')
 
         self.assertEqual(quicksort_array,self.quicksort_result)
     
+    def test_algo_binary_search_no_data(self):
+        response = self.client.post(self.binary_search)
+
+        self.assertEqual(response.data.get('Success'),False)
+
+
     def test_algo_binary_search(self):
         response = self.client.post(self.binary_search,self.binary_search_array_data)
         item_index = response.data.get('item_index')
