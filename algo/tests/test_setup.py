ec2e9a57e9815a60fd32e431af744af1033ec827
josevenzke <josevenzkejr@gmail.com>
2021-06-14 18:54:02 -0300

    better vertical spacing

--- algo/tests/test_setup.py
+++ algo/tests/test_setup.py
@@ -6,18 +6,17 @@ class TestSetUp(APITestCase):
     
     def setUp(self):
         self.quicksort = reverse('quicksort')
-        self.binary_search = reverse('binary-search')
-        
         self.quicksort_array_data={
             'array':'4,6,3,8,2,10,23,1,93,92'
         }
         self.quicksort_result = [1,2,3,4,6,8,10,23,92,93]
 
+
+        self.binary_search = reverse('binary-search')
         self.binary_search_array_data={
             'array':'1,2,3,5,7,8,21,35,36,97,203,504,607',
             'item':'3'
         }
-
         self.binary_search_result = 2
 
         return super().setUp()
